777231f8c0d2e4b5386e1e70129cb60a0bb7dcb5
neerajsonii <nrsoni1992@@gmail.com>
2021-06-01 15:12:56 +0530

    husky, pre-commit-hooks added

--- src/app/shared/interfaces/socket.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file contains the code required
- * to define interfaces for Socket related operations.
- */
-import socketio from "socket.io";
-
-export interface Socket extends socketio.Socket {
-    userIdentity?: any;
-}
